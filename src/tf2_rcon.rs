3c31802ffc1624951fed2eca72749e96e55ee55b
Sarah Ohlin <sarahohlin1@gmail.com>
2023-10-31 18:33:38 -0400

    kitty girl TV => tiny kitty TV

--- src/tf2_rcon.rs
+++ src/tf2_rcon.rs
@@ -61,7 +61,10 @@ impl RconController {
     pub async fn status(&mut self) -> Result<GameState, Error> {
         let status_msg = self.run("status").await?;
 
-        let players = Self::parse_player_list(&status_msg)?.into_iter().filter(|p| p.name != "kitty girl TV").collect();
+        let players = Self::parse_player_list(&status_msg)?
+            .into_iter()
+            .filter(|p| p.name != "tiny kitty TV")
+            .collect();
         let map = Self::parse_current_map(&status_msg)?;
 
         Ok(GameState { players, map })
