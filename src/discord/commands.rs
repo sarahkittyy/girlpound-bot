0e9f467783f782f7ab23b602bac4eca21dc34acc
Sarah Ohlin <sarahohlin1@gmail.com>
2023-11-24 14:19:57 -0500

    fix: add quotes to discord ban commands

--- src/discord/commands.rs
+++ src/discord/commands.rs
@@ -83,14 +83,18 @@ pub async fn tf2ban(
     #[description = "The reason for the ban"] reason: Option<String>,
 ) -> Result<(), Error> {
     let reason = reason.unwrap_or("undesirable".to_owned());
-    rcon_and_reply(ctx, format!("sm_ban {} {} {}", username, minutes, reason)).await
+    rcon_and_reply(
+        ctx,
+        format!("sm_ban \"{}\" {} {}", username, minutes, reason),
+    )
+    .await
 }
 
 /// Unban a user from the tf2 server
 #[poise::command(slash_command)]
 pub async fn tf2unban(
     ctx: Context<'_>,
-    #[description = "The steamid / ip to ban."] steamid: String,
+    #[description = "The steamid / ip to unban."] steamid: String,
     #[description = "The reason for the unban"] reason: Option<String>,
 ) -> Result<(), Error> {
     let reason = reason.unwrap_or("chill".to_owned());
@@ -104,8 +108,8 @@ pub async fn tf2kick(
     #[description = "The username to kick."] username: String,
     #[description = "The reason for the kick"] reason: Option<String>,
 ) -> Result<(), Error> {
-    let reason = reason.unwrap_or("".to_owned());
-    rcon_and_reply(ctx, format!("sm_kick {} {}", username, reason)).await
+    let reason = reason.unwrap_or("1984".to_owned());
+    rcon_and_reply(ctx, format!("sm_kick \"{}\" {}", username, reason)).await
 }
 
 /// Mute a user's vc on the tf2 server
@@ -116,9 +120,13 @@ pub async fn tf2mute(
     #[description = "Time to mute them for, in minutes"] minutes: Option<u32>,
     #[description = "The reason for the mute"] reason: Option<String>,
 ) -> Result<(), Error> {
-    let reason = reason.unwrap_or("".to_owned());
+    let reason = reason.unwrap_or("1984".to_owned());
     let minutes = minutes.unwrap_or(0);
-    rcon_and_reply(ctx, format!("sm_mute {} {} {}", username, minutes, reason)).await
+    rcon_and_reply(
+        ctx,
+        format!("sm_mute \"{}\" {} {}", username, minutes, reason),
+    )
+    .await
 }
 
 /// Unmute a user's vc on the tf2 server
@@ -128,8 +136,8 @@ pub async fn tf2unmute(
     #[description = "The username to unmute."] username: String,
     #[description = "The reason for the unmute"] reason: Option<String>,
 ) -> Result<(), Error> {
-    let reason = reason.unwrap_or("".to_owned());
-    rcon_and_reply(ctx, format!("sm_unmute {} {}", username, reason)).await
+    let reason = reason.unwrap_or("vibin".to_owned());
+    rcon_and_reply(ctx, format!("sm_unmute \"{}\" {}", username, reason)).await
 }
 
 /// Gag a user's text chat on the tf2 server
@@ -140,9 +148,13 @@ pub async fn tf2gag(
     #[description = "Time to gag them for, in minutes"] minutes: Option<u32>,
     #[description = "The reason for the gag"] reason: Option<String>,
 ) -> Result<(), Error> {
-    let reason = reason.unwrap_or("".to_owned());
+    let reason = reason.unwrap_or("1984".to_owned());
     let minutes = minutes.unwrap_or(0);
-    rcon_and_reply(ctx, format!("sm_gag {} {} {}", username, minutes, reason)).await
+    rcon_and_reply(
+        ctx,
+        format!("sm_gag \"{}\" {} {}", username, minutes, reason),
+    )
+    .await
 }
 
 /// Ungag a user's text chat on the tf2 server
@@ -153,7 +165,7 @@ pub async fn tf2ungag(
     #[description = "The reason for the ungag"] reason: Option<String>,
 ) -> Result<(), Error> {
     let reason = reason.unwrap_or("".to_owned());
-    rcon_and_reply(ctx, format!("sm_gag {} {}", username, reason)).await
+    rcon_and_reply(ctx, format!("sm_ungag \"{}\" {}", username, reason)).await
 }
 
 fn hhmmss(duration: &Duration) -> String {
