7dc8fdcfbe6945f8e358d50d1626a657427caef2
Sarah Ohlin <sarahohlin1@gmail.com>
2023-12-15 03:01:40 -0500

    feat: detailed catsmas who

--- src/discord/commands/catsmas.rs
+++ src/discord/commands/catsmas.rs
@@ -67,34 +67,56 @@ async fn leave(ctx: Context<'_>) -> Result<(), Error> {
 /// fetch info on who ur secret santa is
 #[poise::command(slash_command)]
 async fn who(ctx: Context<'_>) -> Result<(), Error> {
-    let serenity::UserId(uid) = ctx.author().id;
-    // fetch how many entries there are
-    let ct = sqlx::query!(
-        r#"
-		SELECT COUNT(user_id) AS `ct` FROM `catsmas_users`
-		"#
-    )
-    .fetch_one(&ctx.data().pool)
-    .await?;
-    // check self status
-    let me = sqlx::query!(
-        r#"
-		SELECT *
-		FROM `catsmas_users`
-		WHERE `user_id` = ?
-		"#,
-        uid.to_string()
-    )
-    .fetch_all(&ctx.data().pool)
-    .await?;
+    ctx.defer_ephemeral().await?;
+    let uid = ctx.author().id;
+    // fetch users
+    let user_ids: Vec<serenity::UserId> = sqlx::query!("SELECT * FROM `catsmas_users`")
+        .fetch_all(&ctx.data().pool)
+        .await?
+        .into_iter()
+        .map(|u| serenity::UserId(u.user_id.parse().unwrap()))
+        .collect();
+    let has_self = user_ids.contains(&uid);
+
+    let users = {
+        let (i, mut o) = tokio::sync::mpsc::channel(100);
+        for uid in user_ids {
+            let ctx = ctx.serenity_context().clone();
+            let i = i.clone();
+            tokio::spawn(async move {
+                if let Ok(user) = uid.to_user(&ctx).await {
+                    let _ = i.send(user).await;
+                }
+            });
+        }
+        drop(i);
+        let mut users = Vec::new();
+        while let Some(user) = o.recv().await {
+            users.push(user);
+        }
+        users
+    };
+
     ctx.send(|m| {
         m.embed(|e| {
-            e.title(if !me.is_empty() {
+            e.title(if has_self {
                 "ur enwolled!! prepare to eventually get a gift for someone <3"
             } else {
                 "ur nyot in"
             })
-            .description(format!("thewe r {} users ready 2 gift...", ct.ct))
+            .description(format!(
+                "{} entered:\n```{}```",
+                users.len(),
+                if users.is_empty() {
+                    "nyo one".to_owned()
+                } else {
+                    users
+                        .iter()
+                        .map(|u| format!("{}#{}", u.name, u.discriminator))
+                        .collect::<Vec<_>>()
+                        .join("\n")
+                }
+            ))
             .footer(|f| f.text("/catsmas join  |  /catsmas leave"))
         })
     })
