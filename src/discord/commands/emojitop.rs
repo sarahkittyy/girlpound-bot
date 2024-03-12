0d7cfade0058f797c797a9c549b173ca725fa5ac
Sarah Ohlin <sarahohlin1@gmail.com>
2024-03-12 16:31:13 -0400

    feat: abstract emoji rank sql query, add total uses subcommand

--- src/discord/commands/emojitop.rs
+++ src/discord/commands/emojitop.rs
@@ -1,10 +1,14 @@
 use poise::{serenity_prelude::CreateEmbed, CreateReply};
-use sqlx;
+use sqlx::{self, MySql, Pool};
 
 use crate::{discord::Context, Error};
 
 /// Returns the most used emoji in the server.
-#[poise::command(slash_command, subcommands("text", "react"), subcommand_required)]
+#[poise::command(
+    slash_command,
+    subcommands("text", "react", "total"),
+    subcommand_required
+)]
 pub async fn emojitop(_ctx: Context<'_>) -> Result<(), Error> {
     Ok(())
 }
@@ -19,12 +23,22 @@ fn codepoint_to_emoji(code: String) -> String {
         .collect::<String>()
 }
 
-/// Top used reactions
-#[poise::command(slash_command, user_cooldown = 10)]
-async fn react(ctx: Context<'_>) -> Result<(), Error> {
-    let rows = sqlx::query!("SELECT * FROM `emojirank` ORDER BY `react_count` DESC LIMIT 10")
-        .fetch_all(&ctx.data().local_pool)
-        .await?;
+#[derive(sqlx::FromRow)]
+struct EmojiRankRow {
+    is_discord: i8,
+    animated: i8,
+    name: String,
+    eid: String,
+    count: i64,
+}
+
+async fn get_ranks(order_by: &str, pool: &Pool<MySql>) -> Result<String, Error> {
+    let rows: Vec<EmojiRankRow> = sqlx::query_as(&format!(
+        "SELECT *, {} AS count FROM `emojirank` ORDER BY count DESC LIMIT 10",
+        order_by
+    ))
+    .fetch_all(pool)
+    .await?;
 
     let desc: String = rows
         .into_iter()
@@ -43,15 +57,34 @@ async fn react(ctx: Context<'_>) -> Result<(), Error> {
                 } else {
                     codepoint_to_emoji(row.eid)
                 },
-                row.react_count
+                row.count
             )
         })
         .collect::<Vec<_>>()
         .join("\n");
 
+    Ok(desc)
+}
+
+/// Top used emojis in total
+#[poise::command(slash_command, user_cooldown = 10)]
+async fn total(ctx: Context<'_>) -> Result<(), Error> {
+    let mut embed = CreateEmbed::new();
+    embed = embed.title("Top overall emojis :3");
+    embed = embed.description(get_ranks("use_count+react_count", &ctx.data().local_pool).await?);
+
+    ctx.send(CreateReply::default().embed(embed).ephemeral(true))
+        .await?;
+
+    Ok(())
+}
+
+/// Top used reactions
+#[poise::command(slash_command, user_cooldown = 10)]
+async fn react(ctx: Context<'_>) -> Result<(), Error> {
     let mut embed = CreateEmbed::new();
     embed = embed.title("Top reaction emojis :3");
-    embed = embed.description(desc);
+    embed = embed.description(get_ranks("react_count", &ctx.data().local_pool).await?);
 
     ctx.send(CreateReply::default().embed(embed).ephemeral(true))
         .await?;
@@ -62,36 +95,9 @@ async fn react(ctx: Context<'_>) -> Result<(), Error> {
 /// Top used emojis in messages
 #[poise::command(slash_command, user_cooldown = 10)]
 async fn text(ctx: Context<'_>) -> Result<(), Error> {
-    let rows = sqlx::query!("SELECT * FROM `emojirank` ORDER BY `use_count` DESC LIMIT 10")
-        .fetch_all(&ctx.data().local_pool)
-        .await?;
-
-    let desc: String = rows
-        .into_iter()
-        .enumerate()
-        .map(|(i, row)| {
-            format!(
-                "{}. {} - `{}`",
-                i + 1,
-                if row.is_discord == 1 {
-                    format!(
-                        "<{}:{}:{}>",
-                        if row.animated == 1 { "a" } else { "" },
-                        row.name,
-                        row.eid
-                    )
-                } else {
-                    codepoint_to_emoji(row.eid)
-                },
-                row.use_count
-            )
-        })
-        .collect::<Vec<_>>()
-        .join("\n");
-
     let mut embed = CreateEmbed::new();
     embed = embed.title("Top message emojis :3");
-    embed = embed.description(desc);
+    embed = embed.description(get_ranks("use_count", &ctx.data().local_pool).await?);
 
     ctx.send(CreateReply::default().embed(embed).ephemeral(true))
         .await?;
