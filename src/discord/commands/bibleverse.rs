1c548b9860f8153a68fdf82fcf8eeb6437d266cd
Sarah Ohlin <sarahohlin1@gmail.com>
2024-03-12 00:32:15 -0400

    feat: ultralations

--- src/discord/commands/bibleverse.rs
+++ src/discord/commands/bibleverse.rs
@@ -16,11 +16,22 @@ struct BibleVerseApiResponse {
 
 /// Get a random bible verse!
 #[poise::command(slash_command, channel_cooldown = 10)]
-pub async fn bibleverse(ctx: Context<'_>) -> Result<(), Error> {
+pub async fn bibleverse(
+    ctx: Context<'_>,
+    #[description = "Preach from the book of ultralations."] ultra: Option<bool>,
+) -> Result<(), Error> {
     const URL: &'static str = "https://bible-api.com/?random=verse";
 
     let resp = reqwest::get(URL).await?;
     let verse = resp.json::<BibleVerseApiResponse>().await?;
+
+    let ultra = ultra.unwrap_or(false);
+
+    if ultra {
+        ctx.send(CreateReply::default().content("\"Game is game rise and grind to hustle everyday so that one day you may get yourself a cute trans girl in your dms begging for your presence everyday..\" (Ultralations 6:23)"))
+			.await?;
+        return Ok(());
+    }
 
     let text = if random::<f32>() > 0.7 {
         uwuify_str_sse(&verse.text.trim())
