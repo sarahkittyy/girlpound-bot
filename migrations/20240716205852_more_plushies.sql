a8ea905380d22f02114e515e2dc5da8394505ec9
Sarah Ohlin <sarahohlin1@gmail.com>
2024-07-16 17:01:52 -0400

    feat: more plushies

---
+++ migrations/20240716205852_more_plushies.sql
@@ -0,0 +1,5 @@
+-- Add migration script here
+INSERT INTO `catcoin_reward` (`name`, `file`, `rarity`) VALUES
+("Cute Deer (Defense Position)", "public/deer_defense.png", "Peak"),
+("Cheery Deer (Seated Position)", "public/deer_seated.png", "Common"),
+("Chill Deer (Splooted Position)", "public/deer_splat.png", "Fluffy");
