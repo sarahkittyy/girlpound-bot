f00a44a0f0016783b36f9454d205de372afd5f06
Sarah Ohlin <sarahohlin1@gmail.com>
2024-10-01 16:21:48 -0400

    more plushie drops

---
+++ migrations/20241001195552_more_rewards.sql
@@ -0,0 +1,16 @@
+-- Add migration script here
+INSERT INTO `catcoin_reward` (`name`, `file`, `rarity`) VALUES
+("Brian Griffin (Smug Position)", "public/brian_smug.png", "Common"),
+("Cat Shark (Seated Position)", "public/cat_shark_seated.png", "Common"),
+("Hark (Defense Position)", "public/hark_defense.png", "Common"),
+("Train (Train)", "public/train.png", "Fluffy"),
+("ctf_2fort (Shitty Map Position)", "public/ctf_2fort.png", "Fluffy"),
+("Luna (Seated Position)", "public/luna_seated.png", "Fluffy"),
+("Fluffy Unicorn (Attack Position)", "public/unicorn_attack.png", "Common"),
+("Cat Octopus (Seated Position)", "public/cat_octopus_seated.png", "Common"),
+("Sanrio (Seated Position)", "public/sanrio_seated.png", "Common"),
+("Evil Sanrio (Seated Position)", "public/evil_sanrio_seated.png", "Common"),
+("Pompompurin (Seated Position)", "public/pompompurin_seated.png", "Common"),
+("Minecraft Bee (Attack Position)", "public/bee_attack.png", "Fluffy"),
+("Minecraft Creeper (Attack Position)", "public/creeper_attack.png", "Fluffy"),
+("Minecraft Chicken (Seated Position)", "public/chicken_seated.png", "Fluffy");
